ff43c7c5e2f667095dc0ff4dc08448af30988706
VarleyS <varleysr@gmail.com>
2024-02-18 17:29:59 -0300

    ajuste js para conexão com api

--- js/cadastroAluno.js
+++ js/cadastroAluno.js
@@ -2,22 +2,30 @@ import axios from 'https://cdn.jsdelivr.net/npm/axios@1.3.5/+esm';
 
 async function postAluno(){
     try {
-
         const formulario = document.getElementById('formularioAluno');
-
         const dados = {
-                "name": formulario.name.value,
-                "dataNascimento": formulario.dataNascimento.value,
-                "cpf": formulario.cpf.value,
-                "rg": formulario.rg.value,
-                "sexo": formulario.sexo.value,
-                "telefone": formulario.telefone.value
+                name: formulario.name.value, 
+                dataNascimento: formulario.dataNascimento.value,
+                cpf: formulario.cpf.value,
+                rg: formulario.rg.value,
+                sexo: formulerio.sexo.value,
+                telefone: formulario.telefone.value
             };
             
-        const resposta = await axios.post('https://localhost:7262/aluno', dados);  
+            await axios.post("http://localhost:8082/alunos",dados)
+                      .then(function (response){
+                          console.log(response);
+                           window.location.href="index.html";
+                       })
+        
+    } catch (error) {
+        alert ("Erro ao enviar os dados");
+        console.log(erro);
+    }
+}
+   /*     const resposta = await axios.post('https://localhost:7262/aluno', dados);  
         console.log("Dados do aluno salvo com sucesso!", resposta.data);
     } catch (error) {
         console.log("Erro na requisição POST: ", error);
     }
-}
-console.log(await postAluno());
+}*/
